b9654d9b6d2af230aafc5337c9a145da8a5bd295
marmin-dev <marmin.dev@gmail.com>
2024-05-02 23:08:13 +0900

    module ended

--- nodejs4/controllers/shop.js
+++ nodejs4/controllers/shop.js
@@ -16,28 +16,35 @@ exports.getProducts = (req, res, next) => {
 
 // 메인 페이지
 exports.getIndex = (req, res, next) => {
-    Product.fetchAll((products) => {
-        res.render("shop/index", {
-          prods: products,
-          pageTitle: "Shop",
-          path: "/",
-          hasProducts: products.length > 0,
-          activeShop: true,
-          productCSS: true,
-        });
-      });
-}
+  Product.fetchAll((products) => {
+    res.render("shop/index", {
+      prods: products,
+      pageTitle: "Shop",
+      path: "/",
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true,
+    });
+  });
+};
 
 exports.getCart = (req, res, next) => {
-    res.render('shop/cart', {
-        path: "/cart",
-        pageTitle:'Your Cart',
-    });
-}
+  res.render("shop/cart", {
+    path: "/cart",
+    pageTitle: "Your Cart",
+  });
+};
+
+exports.getOrders = (req, res, next) => {
+  res.render("shop/orders", {
+    path: "/orders",
+    pageTitle: "Your Orders",
+  });
+};
 
 exports.getCheckout = (req, res, next) => {
-    res.render('shop/checkout',{
-        path:'/checkout',
-        pageTitle:'Checkout'
-    })
-}
+  res.render("shop/checkout", {
+    path: "/checkout",
+    pageTitle: "Checkout",
+  });
+};
