ee1547f24a941a9c868abde321e30bc32fad8985
marmin-dev <marmin.dev@gmail.com>
2024-05-10 00:02:43 +0900

    sequelize module end

--- nodejs5/app.js
+++ nodejs5/app.js
@@ -8,6 +8,9 @@ const sequelize = require("./util/database");
 const Product = require("./models/product");
 const User = require("./models/user");
 const Cart = require("./models/cart");
+const CartItem = require("./models/cart-item");
+const Order = require("./models/order");
+const OrderItem = require("./models/order-item");
 
 const app = express();
 
@@ -16,7 +19,6 @@ app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
-const CartItem = require("./models/cart-item");
 
 // sql 을 문자열로 하여 사용이 가능하다.
 // db.execute("SELECT * FROM products")
@@ -50,8 +52,12 @@ User.hasOne(Cart);
 Cart.belongsTo(User);
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
 
 sequelize
+  //   .sync({ force: true })
   .sync()
   .then((result) => {
     return User.findByPk(1);
